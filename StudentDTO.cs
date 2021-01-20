dbbcc61d520ff36262978aacc19e966451de5055
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-01-20 22:08:29 +0530

    Add project files.

---
+++ StudentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace automapper_sample
+{
+    public class StudentDTO
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+       // public AddressDTO Address { get; set; }
+
+    }
+}
